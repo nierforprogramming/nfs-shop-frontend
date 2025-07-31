932e5f24e57b3e670801a558f6f6b2543e272b94
Nier <nierforprogramming@gmail.com>
2025-07-31 09:04:25 +0000

    Latest collection section completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className="flex items-center justify-between py-5 font-bold">
       <div className="logo">
         <Link to="/" className="text-3xl font-medium">
-          NF<span className="text-accent-color">S</span> SHOP
+          NEFO<span className="text-accent-color">SHOP</span>
         </Link>
       </div>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
